d6b0383e30901de27587a9ce09667a828c8d8a95
maikCyphlock <maikolaguilar656@gmail.com>
2024-02-11 20:29:29 -0400

    ✨ feat: add PriceHistroy to save history of dollar.

--- save_to_db.js
+++ save_to_db.js
@@ -1,22 +1,32 @@
 const { getMonitor } = require("consulta-dolar-venezuela");
 const db = require("better-sqlite3")("dolar.db");
 
-const insert = db.prepare(
-  "INSERT INTO Monitors (id, title, price, price_old, type, lastUpdate) VALUES (@id, @title, @price, @price_old, @type, @lastUpdate)"
+const update = db.prepare(
+  "UPDATE Monitors SET title = @title, price = @price, price_old = @price_old, type = @type,lastUpdate = @lastUpdate WHERE id = @id;"
+);
+
+const insertHistory = db.prepare(
+  "INSERT INTO PriceHistory ( monitor_id, price, timestamp) VALUES (@id, @price, @lastUpdate)"
 );
 
 const insertMany = db.transaction((monitors) => {
-  for (const monitor of monitors) insert.run(monitor);
+  for (const monitor of monitors) update.run(monitor);
 });
 
 getMonitor("null").then(($) => {
   Object.entries($).forEach((key) => {
-    insert.run({
+    update.run({
       id: key[0],
       title: key[1].title,
       price: key[1].price,
       price_old: key[1].price_old,
       type: key[1].type,
+      lastUpdate: key[1].lastUpdate,
+    });
+
+    insertHistory.run({
+      id: key[0],
+      price: key[1].price,
       lastUpdate: key[1].lastUpdate,
     });
   });
